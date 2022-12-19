bd5ef71ad184e479f073db81910ebc72350cd8fe
harshithakamana <121012835+harshithakamana@users.noreply.github.com>
2022-12-19 14:34:12 +0530

    Add files via upload

---
+++ multiplication.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class multiplication
+{
+	public static void main(String args[])
+	{
+		int n;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println(n +"*"+i+"="+(n*i));
+		}
+	}
+}
+			
+
+
+
+
+
+
+
+
+
